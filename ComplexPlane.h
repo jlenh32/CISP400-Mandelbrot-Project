ecc6c02918f196ad51774aa0d93b19d9c3abfea0
Jeffrey Lenh <130512182+jlenh32@users.noreply.github.com>
2023-04-25 15:33:36 -0700

    Add files via upload
    header file

---
+++ ComplexPlane.h
@@ -0,0 +1,22 @@
+#ifndef COMPLEXPLANE_H
+#define COMPLEXPLANE_H
+
+using namespace sf;
+class ComplexPlane
+{
+	public: 
+		ComplexPlane(float aspectRatio);
+		void zoomIn();
+		void zoomOut();
+		void setCenter(Vector2f coord);
+		View getView();
+		void setMouseLocation(Vector2f coord);
+		void loadText(Vector2f coord);
+		size_t countIterations(Vector2f coord);
+		void iteratorsToRGB(size_t count, Uint&& r, Uint&& g, Uint&& b);
+	private:
+		Vector2f m_mouseLocation;
+		View m_view;
+		int m_zoomCount;
+		float m_aspectRatio;
+};

91fcbfd805186891fcda23e2e0152a3ef4a777f6
akushnir15@gmail.com <akushnir15@gmail.com>
2023-04-25 17:30:44 -0700

    Add project files.

---
+++ ComplexPlane.cpp
@@ -0,0 +1,68 @@
+#include "ComplexPlane.h"
+using namespace sf;
+using namespace std;
+
+ComplexPlane::ComplexPlane(float aspectRatio)
+{
+	m_aspectRatio = aspectRatio;
+	m_view.setSize(BASE_WIDTH, -BASE_HEIGHT * m_aspectRatio);
+	m_view.setCenter(0.0, 0.0);
+	m_zoomCount = 0;
+}
+
+void ComplexPlane::zoomIn()
+{
+	m_zoomCount++;
+	float xVal = BASE_WIDTH * pow(BASE_ZOOM, m_zoomCount);
+	float yVal = BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_zoomCount);
+	m_view.setSize(xVal, yVal);
+}
+
+void ComplexPlane::zoomOut()
+{
+	m_zoomCount--;
+	float xVal = BASE_WIDTH * pow(BASE_ZOOM, m_zoomCount);
+	float yVal = BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_zoomCount);
+	m_view.setSize(xVal, yVal);
+}
+
+void ComplexPlane::setCenter(Vector2f coord)
+{
+	m_view.setCenter(coord);
+}
+
+void ComplexPlane::setMouseLocation(Vector2f coord)
+{
+	m_mouseLocation = coord;
+}
+
+void ComplexPlane::loadText(Text& text)
+{
+	stringstream ssText;
+	ssText << "Mandelbrot Set\n"
+		<< "Center: (" << m_view.getCenter().x << ", " << m_view.getCenter().y << ")\n"
+		<< "Cursor: (" << m_mouseLocation.x << ", " << m_mouseLocation.y << ")\n"
+		<< "Left-click to zoom in\n"
+		<< "Right-click to zoom out";
+
+	text.setString(ssText.str());
+}
+
+size_t ComplexPlane::countIterations(Vector2f coord)
+{
+	size_t coordIter = 0;
+	complex<double> z(0, 0);
+	complex<double> c(coord.x, coord.y);
+	for (int i = 0; i < MAX_ITER && abs(z) < 2.0; i++)
+	{
+		z = z * z + c;
+		coordIter++;
+	}
+
+	return coordIter;
+}
+
+void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
+{
+
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "ComplexPlane.h"
+
+using namespace sf;
+using namespace std;
+
+int main()
+{
+	ComplexPlane x(1.0);
+	
+	return 0;
+}

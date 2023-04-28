d977210c136a4632a8a20d357f8284b8b840bf39
aa-alan <126564988+aa-alan@users.noreply.github.com>
2023-04-28 01:42:02 -0700

    Add files via upload

--- ComplexPlane.h
+++ ComplexPlane.h
@@ -7,7 +7,7 @@
 using namespace sf;
 
 const unsigned int MAX_ITER = 64;
-const float BASE_WIDTH = 7.0;
+const float BASE_WIDTH = 4.0;
 const float BASE_HEIGHT = 4.0;
 const float BASE_ZOOM = 0.5;
 
